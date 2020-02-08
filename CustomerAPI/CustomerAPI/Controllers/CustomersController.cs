f28223f076f182dbd716cc7228033edf1c67c6c1
caner yalcin <caneryalcin29@hotmail.com>
2020-02-08 10:35:26 +0300

    Customer API

---
+++ CustomerAPI/CustomerAPI/Controllers/CustomersController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CustomerAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CustomerAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly CustomerAPIContext _context;
+        IMemoryCache _memoryCache;
+
+        public CustomersController(CustomerAPIContext context, IMemoryCache memoryCache)
+        {
+            _context = context;
+            _memoryCache = memoryCache;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetCustomer()
+        {
+            const string cacheKey = "customerListKey";
+            if (_memoryCache.TryGetValue(cacheKey, out object list))
+                return Ok(list);
+
+            var response = _context.Customer.Select(x => new
+            {
+                x.Id,
+                x.Name,
+                x.Surname,
+
+            }).ToList();
+
+            var cacheExpirationOptions = new MemoryCacheEntryOptions
+            {
+
+                AbsoluteExpiration = DateTime.Now.AddSeconds(5),
+                Priority = CacheItemPriority.Normal
+
+            };
+            _memoryCache.Set(cacheKey, response, cacheExpirationOptions);
+            return Ok(response);
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomer([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var customer = await _context.Customer.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer([FromRoute] int id, [FromBody] Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Customers
+        [HttpPost]
+        public async Task<IActionResult> PostCustomer([FromBody] Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Customer.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomer", new { id = customer.Id }, customer);
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var customer = await _context.Customer.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customer.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return Ok(customer);
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customer.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CustomerAPI/CustomerAPI/Migrations/20200204182653_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CustomerAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
